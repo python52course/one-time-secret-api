a4de258782a76756042e713de35ffe31512dbaa2
pecheritsa <pecheritsa.roman@gmail.com>
2024-07-29 17:25:10 +0300

    changed model Secret

--- onetimesecret/models.py
+++ onetimesecret/models.py
@@ -6,15 +6,16 @@ from typing import Optional
 class Secret(BaseModel):
     """
     Represents a secret entity with associated metadata.
+
     Attributes:
-        id (str): Unique identifier for the secret.
+        id (Optional[str]): Unique identifier for the secret. Optional field.
         secret (str): The actual secret data, which will be hashed.
         passphrase (str): The passphrase to access the secret, which will be hashed.
         secret_key (str): A unique key that is used to retrieve the secret.
         expiration (Optional[datetime]): The expiration date and time for the secret.
     """
-    id: str
+    id: Optional[str] = None
     secret: str
     passphrase: str
     secret_key: str
-    expiration: Optional[datetime]
+    expiration: Optional[datetime] = None
