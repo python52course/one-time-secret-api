8ae6a6ae19074550048a42afd5329f8a18986465
pecheritsa <pecheritsa.roman@gmail.com>
2024-07-30 18:09:39 +0300

    DEV-32 changed generate_secret_key

--- onetimesecret/utils.py
+++ onetimesecret/utils.py
@@ -1,8 +1,9 @@
 import random
 from string import ascii_letters, digits
 
+selection = ascii_letters + digits
+
 
 def generate_secret_key():
-    selection = ascii_letters + digits
-    return ''.join(random.choice(selection) for _ in range(6))
-
+    key = ''.join(random.choice(selection) for _ in range(6))
+    return key
