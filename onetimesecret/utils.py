6254f644f5f330d74934aab42f7250182cd69d1c
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-01 21:05:04 +0300

    DEV-32 linter module, add docs func generate

--- onetimesecret/utils.py
+++ onetimesecret/utils.py
@@ -1,9 +1,16 @@
 import random
 from string import ascii_letters, digits
 
-selection = ascii_letters + digits
+SELECTION = ascii_letters + digits
 
 
 def generate_secret_key():
-    key = ''.join(random.choice(selection) for _ in range(6))
+    """
+    Generates a random secret key consisting of ASCII letters and digits.
+    The secret key is a string of 6 characters long,
+    randomly selected from the combination of ASCII letters (both lowercase and uppercase) and digits.
+    Returns:
+        str: A randomly generated secret key of length 6.
+    """
+    key = ''.join(random.choice(SELECTION) for _ in range(6))
     return key
