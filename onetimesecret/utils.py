a45684dbd0462dbf58c87104cace5304a22a9ac4
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-14 13:48:14 +0300

    DEV-34 changed utils

--- onetimesecret/utils.py
+++ onetimesecret/utils.py
@@ -1,5 +1,4 @@
 import os
-import random
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 from string import ascii_letters, digits
 
@@ -10,18 +9,6 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
 SELECTION = ascii_letters + digits
-
-
-def generate_secret_key():
-    """
-    Generates a random secret key consisting of ASCII letters and digits.
-    The secret key is a string of 6 characters long,
-    randomly selected from the combination of ASCII letters (both lowercase and uppercase) and digits.
-    Returns:
-        str: A randomly generated secret key of length 6.
-    """
-    key = ''.join(random.choice(SELECTION) for _ in range(6))
-    return key
 
 
 def derive_key(master_key: bytes, salt: bytes) -> bytes:
