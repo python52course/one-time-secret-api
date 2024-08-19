2bd77244ee62120904477626953fda0c4e1394eb
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-19 12:41:16 +0300

    DEV-34 changed class service

--- onetimesecret/services.py
+++ onetimesecret/services.py
@@ -1,55 +1,98 @@
-import os
 import uuid
+from datetime import datetime
 from typing import Optional
 
-from dotenv import load_dotenv
+from cryptography.fernet import InvalidToken
+from fastapi import HTTPException, status
+
 from onetimesecret.database import FakeRepository
-from onetimesecret.utils import encrypt, decrypt
-from onetimesecret.models import Secret, SecretRequest, PassphraseRequest
-
-
-class InvalidSecretKeyException(Exception):
-    pass
-
-
-class InvalidPassphraseException(Exception):
-    pass
+from onetimesecret.models import Secret
+from onetimesecret.utils import decrypt, encrypt, generate_key_from_passphrase
 
 
 class SecretService:
-    def __init__(self):
-        load_dotenv()
-        master_key_str = os.getenv("MASTER_KEY")
-        if master_key_str is None:
-            raise ValueError("MASTER_KEY not found in environment variables.")
-        self.master_key = master_key_str.encode('utf-8')
-        self.repository = FakeRepository()
+    """
+    Service class responsible for handling the creation, retrieval, and deletion of secrets.
 
-    async def generate_secret(self, request: SecretRequest) -> str:
-        encrypted_secret = encrypt(request.secret, self.master_key)
-        encrypted_passphrase = encrypt(request.passphrase, self.master_key)
+    Attributes:
+        salt (bytes): The salt used for key derivation.
+        repository (Repository): The repository where secrets are stored.
+    """
 
+    def __init__(self, salt: str, repository: 'FakeRepository') -> None:
+        """
+        Initializes the SecretService with the provided salt and repository.
+
+        Args:
+            salt (str): The salt used for key derivation.
+            repository (Repository): The repository instance for storing and retrieving secrets.
+        """
+        self.salt = salt.encode()
+        self.repository = repository
+
+    def generate_key(self, passphrase: str) -> bytes:
+        """
+        Generates a key for encryption based on the provided passphrase.
+
+        Args:
+            passphrase (str): The passphrase used for key derivation.
+
+        Returns:
+            bytes: The generated encryption key.
+        """
+        return generate_key_from_passphrase(passphrase.encode(), self.salt)
+
+    async def generate_secret(self, secret: str, passphrase: str) -> str:
+        """
+        Encrypts and stores a secret in the repository.
+
+        Args:
+            secret (str): The secret to be encrypted and stored.
+            passphrase (str): The passphrase used to encrypt the secret.
+
+        Returns:
+            str: A unique identifier for the stored secret.
+        """
+        key = self.generate_key(passphrase)
+        encrypted_secret = encrypt(secret, key)
         secret_key = str(uuid.uuid4())
-
-        secret = Secret(
+        secret_instance = Secret(
+            id=secret_key,
             secret=encrypted_secret,
-            passphrase=encrypted_passphrase,
-            secret_key=secret_key,
-            expiration=request.expiration
+            expiration=datetime.now()
         )
-
-        await self.repository.create(secret)
+        await self.repository.create(secret_instance)
         return secret_key
 
-    async def get_secret(self, secret_key: str, request: PassphraseRequest) -> Optional[str]:
+    async def get_secret(self, secret_key: str, passphrase: str) -> Optional[str]:
+        """
+        Retrieves and decrypts a secret from the repository.
+
+        Args:
+            secret_key (str): The unique identifier of the secret to retrieve.
+            passphrase (str): The passphrase used to decrypt the secret.
+
+        Returns:
+            Optional[str]: The decrypted secret if retrieval is successful, otherwise None.
+
+        Raises:
+            HTTPException: If the secret is not found or decryption fails.
+        """
         secret = await self.repository.get(secret_key)
-        if not secret:
-            raise InvalidSecretKeyException
+        if secret is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="There is no such secret"
+            )
 
-        decrypted_passphrase = decrypt(secret.passphrase, self.master_key)
-        if request.passphrase != decrypted_passphrase:
-            raise InvalidPassphraseException
+        key = self.generate_key(passphrase)
+        try:
+            decrypted_secret = decrypt(secret, key)
+        except InvalidToken:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid input data"
+            )
 
-        decrypted_secret = decrypt(secret.secret, self.master_key)
         await self.repository.delete(secret_key)
         return decrypted_secret
