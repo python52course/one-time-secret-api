edf0af17af4ec85e25ac4d0909e308c1cea1d087
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-22 12:28:01 +0300

    DEV-34 add docs test

--- tests/test_service.py
+++ tests/test_service.py
@@ -7,6 +7,15 @@ from onetimesecret.database import FakeRepository
 
 @pytest.fixture
 def secret_service():
+    """
+    Fixture to set up the SecretService with a fake repository.
+
+    This fixture initializes the `SecretService` with a predefined salt and a `FakeRepository` instance.
+    It provides a clean and consistent environment for testing.
+
+    Returns:
+        SecretService: An instance of the `SecretService` class.
+    """
     salt = "my_secret_salt"
     repository = FakeRepository()
     return SecretService(salt=salt, repository=repository)
@@ -14,6 +23,15 @@ def secret_service():
 
 @pytest.mark.asyncio
 async def test_generate_secret(secret_service):
+    """
+    Test the generation of a new secret.
+
+    This test verifies that a secret can be successfully generated and stored. It checks that the returned
+    secret key is a string with the expected length of 36 characters.
+
+    Args:
+        secret_service (SecretService): An instance of the `SecretService` class provided by the fixture.
+    """
     secret = "my_secret_data"
     passphrase = "my_secret_passphrase"
 
@@ -25,6 +43,15 @@ async def test_generate_secret(secret_service):
 
 @pytest.mark.asyncio
 async def test_get_secret(secret_service):
+    """
+   Test retrieval of a secret.
+
+   This test ensures that a previously generated secret can be retrieved successfully. It checks that the
+   retrieved secret matches the originally stored secret using the correct passphrase.
+
+   Args:
+       secret_service (SecretService): An instance of the `SecretService` class provided by the fixture.
+   """
     secret = "my_secret_data"
     passphrase = "my_secret_passphrase"
 
@@ -36,6 +63,15 @@ async def test_get_secret(secret_service):
 
 @pytest.mark.asyncio
 async def test_get_secret_with_invalid_passphrase(secret_service):
+    """
+    Test retrieval of a secret with an invalid passphrase.
+
+    This test verifies that attempting to retrieve a secret with an incorrect passphrase raises an `HTTPException`
+    with a status code of 400 and an appropriate error detail message.
+
+    Args:
+        secret_service (SecretService): An instance of the `SecretService` class provided by the fixture.
+    """
     secret = "my_secret_data"
     passphrase = "my_secret_passphrase"
     wrong_passphrase = "wrong_passphrase"
@@ -51,6 +87,15 @@ async def test_get_secret_with_invalid_passphrase(secret_service):
 
 @pytest.mark.asyncio
 async def test_get_secret_not_found(secret_service):
+    """
+    Test retrieval of a non-existent secret.
+
+    This test ensures that attempting to retrieve a secret with a non-existent secret key raises an `HTTPException`
+    with a status code of 404 and an appropriate error detail message.
+
+    Args:
+        secret_service (SecretService): An instance of the `SecretService` class provided by the fixture.
+    """
     secret_key = "non_existent_secret_key"
     passphrase = "my_secret_passphrase"
 
@@ -63,6 +108,15 @@ async def test_get_secret_not_found(secret_service):
 
 @pytest.mark.asyncio
 async def test_secret_deleted_after_retrieval(secret_service):
+    """
+    Test that a secret is deleted after retrieval.
+
+    This test verifies that after a secret is retrieved, it is removed from the repository. It ensures that
+    attempting to fetch the secret again returns `None`, indicating the secret was successfully deleted.
+
+    Args:
+        secret_service (SecretService): An instance of the `SecretService` class provided by the fixture.
+    """
     secret = "my_secret_data"
     passphrase = "my_secret_passphrase"
 
