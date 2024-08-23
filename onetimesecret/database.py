9003e08247bbc2facd0b1e7c08bdf97c1fa77f9d
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-23 20:35:37 +0300

    DEV-41 add class Repository -> motor MongoDB

--- onetimesecret/database.py
+++ onetimesecret/database.py
@@ -1,58 +1,67 @@
-from typing import List, Optional
+from typing import Optional
+
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from onetimesecret.models import Secret
 
 
-class FakeRepository:
-    """
-    A fake implementation of a repository for storing secrets in memory.
-    This is primarily used for testing purposes without the need for a real database.
-
-    Attributes:
-        data (List[Secret]): A list that stores secret objects.
-    """
-
-    def __init__(self) -> None:
+class Repository:
+    def __init__(self, uri: str):
         """
-        Initializes the FakeRepository with an empty list to hold secret data.
+        Initializes a new instance of the Repository class.
+
+        Args:
+            uri (str): The URI for connecting to the MongoDB database.
         """
-        self.data: List[Secret] = []
+        self.__client = AsyncIOMotorClient(uri)
+        self.__db = self.__client['secret_db']
+        self.__collection = self.__db['secrets']
+
+    async def initialize_indexes(self):
+        """
+        Initializes indexes in the MongoDB collection.
+        Creates an index on the 'expiration' field with an expiration time of 1 second if it does not already exist.
+        """
+        existing_indexes = await self.__collection.index_information()
+        if 'expiration_1' not in existing_indexes:
+            await self.__collection.create_index([('expiration', 1)], expireAfterSeconds=1)
+
+    async def close(self):
+        """
+        Closes the connection to the MongoDB client.
+        """
+        self.__client.close()
 
     async def create(self, secret: Secret) -> None:
         """
-        Stores a new secret in the repository.
+        Inserts a new secret document into the collection. Uses the 'secret_key' field
+        as the '_id' field in the MongoDB document.
 
         Args:
-            secret (Secret): The secret object to be stored.
-
-        Returns:
-            None
+            secret (Secret): The secret object to be inserted, with fields 'id', 'secret', and 'expiration'.
         """
-        self.data.append(secret)
+        secret_dict = secret.model_dump()
+        secret_dict['_id'] = secret_dict.pop('secret_key')
+        await self.__collection.insert_one(secret_dict)
 
     async def get(self, secret_key: str) -> Optional[str]:
         """
-        Retrieves a secret from the repository using its unique secret key.
+        Retrieves a secret from the collection using the secret key (which is used as '_id').
 
         Args:
-            secret_key (str): The unique identifier of the secret to retrieve.
+            secret_key (str): The key of the secret to retrieve, which corresponds to '_id' in MongoDB.
 
         Returns:
-            Optional[str]: The decrypted secret if found, otherwise None.
+            Optional[str]: The secret if found, otherwise None.
         """
-        for item in self.data:
-            if item.id == secret_key:
-                return item.secret
-        return None
+        secret = await self.__collection.find_one({"_id": secret_key})
+        return secret["secret"] if secret else None
 
     async def delete(self, secret_key: str) -> None:
         """
-        Deletes a secret from the repository using its unique secret key.
+        Deletes a secret from the collection using the secret key (which is used as '_id').
 
         Args:
-            secret_key (str): The unique identifier of the secret to delete.
-
-        Returns:
-            None
+            secret_key (str): The key of the secret to delete, which corresponds to '_id' in MongoDB.
         """
-        self.data = [item for item in self.data if item.id != secret_key]
+        await self.__collection.delete_one({"_id": secret_key})
