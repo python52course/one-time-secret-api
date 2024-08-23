669d9150f20576d17f4eea14dbc75d5ba97f070b
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-23 20:41:23 +0300

    DEV-41 add saynccontextlib for startup project, add depends, restructure project

--- onetimesecret/main.py
+++ onetimesecret/main.py
@@ -1,9 +1,83 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-from onetimesecret.routes import route
+from fastapi import FastAPI, Depends
+
+from onetimesecret.services import SecretService
+from onetimesecret.models import PassphraseRequest, SecretRequest, SecretResponse, SecretKeyResponse
+from onetimesecret.database import Repository
+from onetimesecret.config import uri, salt
 
 
-app = FastAPI(title="One Time Secret")
-app.include_router(route)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Endpoint to generate a new secret.
+
+    Args:
+        request (SecretRequest): The request object containing the secret content and passphrase.
+
+    Returns:
+        SecretKeyResponse: Response object containing the generated secret key.
+
+    Raises:
+        HTTPException: If the request data is invalid or secret generation fails.
+    """
+    repository = Repository(uri)
+    await repository.initialize_indexes()
+
+    secret_service = SecretService(salt, repository)
+
+    app.state.secret_service = secret_service
+
+    try:
+        yield
+    finally:
+        await repository.close()
+
+app = FastAPI(lifespan=lifespan, title="One Time Secret")
 
 
+@app.post("/generate", response_model=SecretKeyResponse)
+async def generate_secret(
+        request: SecretRequest,
+        secret_service: SecretService = Depends(lambda: app.state.secret_service)
+) -> SecretKeyResponse:
+    """
+    Endpoint to generate a new secret.
+
+    Args:
+        request (SecretRequest): The request object containing the secret content and passphrase.
+
+    Returns:
+        SecretKeyResponse: Response object containing the generated secret key.
+
+    Raises:
+        HTTPException: If the request data is invalid or secret generation fails.
+    """
+
+    secret_key = await secret_service.generate_secret(request.secret, request.passphrase)
+    return SecretKeyResponse(secret_key=secret_key)
+
+
+@app.post("/secrets/{secret_key}", response_model=SecretResponse)
+async def get_secret(
+        secret_key: str,
+        request: PassphraseRequest,
+        secret_service: SecretService = Depends(lambda: app.state.secret_service)
+) -> SecretResponse:
+    """
+    Endpoint to retrieve a secret using a secret key.
+
+    Args:
+        secret_key (str): The unique key of the secret to retrieve.
+        request (PassphraseRequest): The request object containing the passphrase for decryption.
+
+    Returns:
+        SecretResponse: Response object containing the decrypted secret.
+
+    Raises:
+        HTTPException: If the secret is not found or the passphrase is incorrect.
+    """
+
+    secret = await secret_service.get_secret(secret_key, request.passphrase)
+    return SecretResponse(secret=secret)
